5179306b79eb7355b38e1a1786c8275bec174397
yq <yuequanhuabai@qq.com>
2021-03-13 23:34:54 +0800

    add a properties

---
+++ src/com/at/pojo/Person.java
@@ -0,0 +1,12 @@
+package com.at.pojo;
+
+/**
+ * @ClassName Person
+ * @Description 描述
+ * @Author YuequanHuabai
+ * @Date 2021/3/13 23:34
+ * @Version 1.0
+ **/
+public class Person {
+    private int age;
+}
