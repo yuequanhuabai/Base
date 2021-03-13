05cda9e7b95f93dc407935b0f23493d950bd0711
yq <yuequanhuabai@qq.com>
2021-03-13 23:20:08 +0800

    practice demo

---
+++ src/Base0001.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Base0001
+ * @Description 描述
+ * @Author YuequanHuabai
+ * @Date 2021/3/13 23:18
+ * @Version 1.0
+ **/
+public class Base0001 {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
