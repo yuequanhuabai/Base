749756474b772db89e70b6d568a820e1d8fd938e
yq <yuequanhuabai@qq.com>
2021-03-14 15:06:08 +0800

    github ...

--- practice/src/com/haha/test/Demo.java
+++ practice/src/com/haha/test/Demo.java
@@ -1,9 +1,15 @@
-package com.haha.test;/**
-* @ClassName Demo
-* @Description 描述
-* @Author YuequanHuabai
-* @Date 2021/3/14 14:34
-* @Version 1.0
-**/
+package com.haha.test;
+
+/**
+ * @ClassName Demo
+ * @Description 描述
+ * @Author YuequanHuabai
+ * @Date 2021/3/14 14:34
+ * @Version 1.0
+ **/
 public class Demo {
+    public static void main(String[] args) {
+        System.out.println(" hello git!");
+        System.out.println("github ....");
+    }
 }
